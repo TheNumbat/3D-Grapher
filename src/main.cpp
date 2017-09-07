e9f307363a4987a705ad2b5afeb4c73c9c77b971
TheNumbat <mslater@nevada.unr.edu>
2017-09-07 14:48:56 -0700

    todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,6 @@
 ﻿
 #include "state.h"
 
-// TODO:
-	// Bugs
-		// Rects have to be set every frame
-		// One strip of polygons is too dark on cyl/spr
-	// Math Features
-		// Graph types (see enum)
-		// Plot evolution of 3D graph over time (4D) (animated)
-		// Highlight curve along a set x/y
-		// Partial Derivatives
-		// Graph intersections
-		// Level Curves
-		// E Regions
-		// Tangent Planes
-		// More than urnary functions (e.g. max, min)
-		// Maxs & mins (abs and rel)
-		// Line/Surface/Flux integrals
-	// Input
-		// Picking
-
 int main(int argc, char** args) {
 
 	state st;
