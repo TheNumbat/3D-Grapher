4a36a012ab02e8057a858de0a31fe009e45a18e1
TheNumbat <mslater@nevada.unr.edu>
2017-09-11 17:14:27 -0700

    clean.bat

--- bin/clean.bat
+++ bin/clean.bat
@@ -2,7 +2,6 @@
 
 pushd w:\build
 
-del w:\data\assets\assets.asset
 del *.ilk > NUL 2> NUL
 del *.dll > NUL 2> NUL
 del *.h > NUL 2> NUL
@@ -11,7 +10,6 @@ del *.obj > NUL 2> NUL
 del *.exe > NUL 2> NUL
 del *.pdb > NUL 2> NUL
 del *.opensdf > NUL 2> NUL
-del *.txt > NUL 2> NUL
 del *.exp > NUL 2> NUL
 del *.lib > NUL 2> NUL
 del *.map > NUL 2> NUL
